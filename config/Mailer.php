daed018708dbf463afdb20a8d79c4829991f424c
Fernando Giardina <fgiardina@outlook.com>
2018-12-29 03:16:56 -0300

    config in src

--- config/Mailer.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'views_folder' => env('MAILER_VIEWS_FOLDER', 'emails.templates')
-];
---
+++ src/config/Mailer.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'views_folder' => env('MAILER_VIEWS_FOLDER', 'emails.templates'),
+];
